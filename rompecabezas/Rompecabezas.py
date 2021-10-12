1b2ff7bbb605df0c0461682bce87e68bb4117564
pablonoya <pablonoya18@live.com>
2021-10-11 20:22:05 -0400

    Rompecabezas.py modificado

--- rompecabezas/Rompecabezas.py
+++ rompecabezas/Rompecabezas.py
@@ -1,25 +1,27 @@
 from busquedas_02 import aestrella, ProblemaBusqueda
 
-OBJETIVO = '''1-2-3
-4-5-6
-7-8-e'''
+OBJETIVO = """1-2-3-a
+4-5-6-b
+7-8-9-c
+d-g-f-e"""
 
-INICIAL = '''1-2-e
-3-4-5
-6-7-8'''
+INICIAL = """2-e-3-a
+1-5-9-6
+4-7-8-b
+d-g-f-c"""
 
 
 def list_to_string(list_):
-    return '\n'.join(['-'.join(row) for row in list_])
+    return "\n".join(["-".join(row) for row in list_])
 
 
 def string_to_list(string_):
-    return [row.split('-') for row in string_.split('\n')]
+    return [row.split("-") for row in string_.split("\n")]
 
 
 def find_location(filas, element_to_find):
-    '''Encuentra la ubicacion de una pieza en el rompecabezas.
-       DEvuelve una tupla: fila, columna'''
+    """Encuentra la ubicacion de una pieza en el rompecabezas.
+    DEvuelve una tupla: fila, columna"""
     for ir, row in enumerate(filas):
         for ic, element in enumerate(row):
             if element == element_to_find:
@@ -28,69 +30,68 @@ def find_location(filas, element_to_find):
 
 posiciones_objetivo = {}
 filas_objetivo = string_to_list(OBJETIVO)
-for numero in '12345678e':
+for numero in "123456789abcdefg":
     posiciones_objetivo[numero] = find_location(filas_objetivo, numero)
 
 
 class EigthPuzzleProblem(ProblemaBusqueda):
     def acciones(self, estado):
-        '''Devuelve una lista de piesas que se pueden mover a un espacio vacio.'''
+        """Devuelve una lista de piesas que se pueden mover a un espacio vacio."""
         filas = string_to_list(estado)
-        fila_e, columna_e = find_location(filas, 'e')
+        fila_e, columna_e = find_location(filas, "e")
 
         acciones = []
         if fila_e > 0:
             acciones.append(filas[fila_e - 1][columna_e])
-        if fila_e < 2:
+        if fila_e < 3:
             acciones.append(filas[fila_e + 1][columna_e])
         if columna_e > 0:
             acciones.append(filas[fila_e][columna_e - 1])
-        if columna_e < 2:
+        if columna_e < 3:
             acciones.append(filas[fila_e][columna_e + 1])
 
         return acciones
 
     def resultado(self, estado, accion):
-        '''Devuelve el resultado despues de mover una pieza a un espacio en vacio
-        '''
+        """Devuelve el resultado despues de mover una pieza a un espacio en vacio"""
         filas = string_to_list(estado)
-        fila_e, columna_e = find_location(filas, 'e')
+        fila_e, columna_e = find_location(filas, "e")
         fila_n, columna_n = find_location(filas, accion)
 
-        filas[fila_e][columna_e], filas[fila_n][columna_n] = filas[fila_n][columna_n], filas[fila_e][columna_e]
+        filas[fila_e][columna_e], filas[fila_n][columna_n] = (
+            filas[fila_n][columna_n],
+            filas[fila_e][columna_e],
+        )
 
         return list_to_string(filas)
 
     def es_objetivo(self, estado):
-        '''Devuelve True si un estado es el estado_objetivo.'''
+        """Devuelve True si un estado es el estado_objetivo."""
         return estado == OBJETIVO
 
     def costo(self, estado1, accion, estado2):
-        '''Devuelve el costo de ejecutar una accion. 
-        '''
+        """Devuelve el costo de ejecutar una accion."""
         return 1
 
     def heuristica(self, estado):
-        '''Devuelve una estimacion de la distancia
+        """Devuelve una estimacion de la distancia
         de un estado a otro, utilizando la distancia manhattan.
-        '''
-        filas = string_to_list(estado)
-
+        """
+        matriz = string_to_list(estado)
         distancia = 0
 
-        for numero in '12345678e':
-            fila_n, columna_n = find_location(filas, numero)
+        for numero in "123456789abcdefg":
+            # indice actual
+            fila_n, columna_n = find_location(matriz, numero)
+            # indice objetivo
             fila_n_objetivo, col_n_goal = posiciones_objetivo[numero]
 
-            distancia += abs(fila_n - fila_n_objetivo) + abs(columna_n - col_n_goal)
-
+            distancia += (fila_n - fila_n_objetivo) ** 2 + (columna_n - col_n_goal) ** 2
         return distancia
 
 
 resultado = aestrella(EigthPuzzleProblem(INICIAL))
 
 for accion, estado in resultado.camino():
-    print('Move numero', accion)
+    print("Move numero", accion)
     print(estado)
-
-
